53a2975cc1bdd3fd63fd9d368be5232d782a8054
Cristian Ismael Gramajo <cristiangramajo015@gmail.com>
2024-08-26 22:53:59 -0300

    cambios

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "Alfrente-v2";
+const CACHE_NAME = "Alfrente-v3";
 const urlsToCache = ["./", "./index.html","./config.html", "./info.html", "./alumnos.html", "./style.css", "./app.js"];
 
 self.addEventListener("install", event => {
